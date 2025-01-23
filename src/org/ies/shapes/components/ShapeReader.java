9d279a099071cf3f38d812dde59f8386f2e8257e
Allan <allanglopez1312@gmail.com>
2025-01-23 11:13:21 +0100

    He creado el constructor

--- src/org/ies/shapes/components/ShapeReader.java
+++ src/org/ies/shapes/components/ShapeReader.java
@@ -1,4 +1,19 @@
 package org.ies.shapes.components;
 
+import org.ies.shapes.model.TriangleRectangle;
+
+import java.util.Scanner;
+
 public class ShapeReader {
+    private final SquareReader squareReader;
+    private final CircleReader circleReader;
+    private final TriangleRectangleReader triangleRectangleReader;
+    private final Scanner scanner;
+
+    public ShapeReader(SquareReader squareReader, CircleReader circleReader, TriangleRectangleReader triangleRectangleReader, Scanner scanner) {
+        this.squareReader = squareReader;
+        this.circleReader = circleReader;
+        this.triangleRectangleReader = triangleRectangleReader;
+        this.scanner = scanner;
+    }
 }
